bfb8bd5ce1464bdd15181be2ec8013964900a58a
Austin Hooker <ahooker@elevenfifty.org>
2022-05-04 15:48:03 -0400

    Initial

---
+++ Models/Pizza.cs
@@ -0,0 +1,9 @@
+namespace PizzAPI.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
